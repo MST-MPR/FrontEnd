183b33fbac994254b7c6e011ab2d6d3ed5455f81
Miguel Portero <migporuiz@gmail.com>
2022-11-15 16:50:29 +0100

    Carousel

--- src/views/Home/HomeIndexView.vue
+++ src/views/Home/HomeIndexView.vue
@@ -1,40 +1,59 @@
+<script setup>
+import Carousel from "@/components/app/AppCarousel.vue";
+</script>
+
 <template>
-  <div class="bg-mst_bg">
-    <div class="basis-1/2 visible lg:hidden">
-      <img
-        class="saturate-50 brightness-50 rounded-bl-[100px]"
-        src="@/assets/images/kameras.jpg"
-        alt="kameras-medien-systemtechnik"
-      />
-    </div>
-    <div
-      class="
-        flex flex-col
-        sm:flex-row
-        text-center
-        drop-shadow-2xl
-        text-mst_white
-      "
-    >
-      <div class="basis-1/2 flex flex-col font-sans tracking-wider">
-        <h1 class="text-center italic font-bold text-2xl mt-10 mb-10 sm:text-6xl sm:mt-10 sm:p-28">
-          Professionell Event... <br />
-          professionell gestreamt...
-        </h1>
-        <h3 class="basis-1/2 ml-15 italic text-sm sm:text-2xl pb-5 pl-5 pr-5">
-          Wir bieten nicht nur Planung und Betreuung von Veranstaltungen an,
-          sondern vermieten und/oder verkaufen auch das notwendige Equipment
-          dafür. Auch hierbei stehen wir Ihnen gern beratend zur Seite und
-          bieten hochwertige Technik zu fairen Preisen.
-        </h3>
-      </div>
-      <div class="basis-1/2 hidden lg:block">
+  <div>
+    <div class="bg-mst_bg h-[930px]">
+      <div class="basis-1/2 visible lg:hidden">
         <img
-          class="saturate-50 brightness-50 rounded-bl-[1200px]"
+          class="saturate-50 brightness-50 rounded-bl-[100px]"
           src="@/assets/images/kameras.jpg"
           alt="kameras-medien-systemtechnik"
         />
       </div>
+      <div
+        class="
+          flex flex-col
+          sm:flex-row
+          text-center
+          drop-shadow-2xl
+          text-mst_white
+        "
+      >
+        <div class="basis-1/2 flex flex-col font-sans tracking-wider">
+          <h1
+            class="
+              text-center
+              italic
+              font-bold
+              text-2xl
+              mt-10
+              mb-10
+              sm:text-6xl sm:mt-10 sm:p-28
+            "
+          >
+            Professionell Event... <br />
+            professionell gestreamt...
+          </h1>
+          <h3 class="basis-1/2 ml-15 italic text-sm sm:text-2xl pb-5 pl-5 pr-5">
+            Wir bieten nicht nur Planung und Betreuung von Veranstaltungen an,
+            sondern vermieten und/oder verkaufen auch das notwendige Equipment
+            dafür. Auch hierbei stehen wir Ihnen gern beratend zur Seite und
+            bieten hochwertige Technik zu fairen Preisen.
+          </h3>
+        </div>
+        <div class="basis-1/2 hidden lg:block">
+          <img
+            class="saturate-50 brightness-50 rounded-bl-[1200px]"
+            src="@/assets/images/kameras.jpg"
+            alt="kameras-medien-systemtechnik"
+          />
+        </div>
+      </div>
+    </div>
+    <div>
+      <Carousel />
     </div>
   </div>
 </template>
