6134113b7afe9cad17b00ab984d1ce8ee00ea4b6
Miguel Portero <migporuiz@gmail.com>
2022-11-17 15:11:09 +0100

    Grid finished & Traductions

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -4,39 +4,57 @@ const i18n = createI18n({
     locale: "Deutsch",
     messages: {
         Deutsch: {
-            /* Navbar */
+            /* HOME*/
+            fullServiceGrid: "Wir verstehen uns als Full-Service-Dienstleister, d.h. wir stellen nicht nur die notwendigen technischen Geräte zur Verfügung, sondern sorgen auch dafür, dass sie perfekt für den jeweiligen Anlass installiert werden.",
+            verkaufGrid: "Nutzen Sie unsere kostenlose unverbindliche Bedarfsanalyse vor Ort. Wir erstellen Ihnen Ihre individuelle Lösung und machen Ihnen einen guten Preis. Bevorzugen Sie ein superaktuelles Neugerät oder tut es auch ein gepflegtes Gebrauchtes?.",
+            vermietungGrid: "Seien Sie gewiß, dass die zu leihenden Geräte immer in einwandfreiem Zustand sind. Alle Mietgegenstände werden regelmäßig gewartet. Die Mietpreise verstehen sich zzgl. der gesetzlichen MwSt., ab unserem Lager.",
+            streamingGrid: "Wir entwickeln und gestalten Ihr individuelles Portal mit Log-In Zertifizierung und weiteren technischen Finessen für einen problemlosen, attraktiven Stream ihres Events.",
+            smartHomeGrid: "Wir automatisieren ihr Privat- oder Bürogebäude von der zentralen Lichtsteuerung bis hin zum komplett smart aufgebautem Gebäude. Hierbei setzen wir primär auf LOXONE – Home Server und entsprechende Peripheriegeräte.",
+            prof1: "Professionell Event...",
+            prof2: "professionell gestreamt...",
+            prof3: "Wir bieten nicht nur Planung und Betreuung von Veranstaltungen an, sondern vermieten und/oder verkaufen auch das notwendige Equipment dafür. Auch hierbei stehen wir Ihnen gern beratend zur Seite und bieten hochwertige Technik zu fairen Preisen.",
+            
+            /* BUTTONS */
             verkauf: "Verkauf",
             vermietung: "Vermietung",
             kontakt: "Kontakt",
             impressum: "Impressum",
             cookieRichtline: "Cookie Richtline (EU)",
 
-            /* Footer */
-            philosophy:' "Wir sind ein Unternehmen, das seine Tätigkeit im Dienstleistungssektor entwickelt, wir widmen uns hauptsächlich der Gestaltung und Produktion von Szenen, audiovisuellen Produkten, Musik und Veranstaltungen, bei denen eine Vielzahl menschlicher und technologischer Faktoren beteiligt sind, die effizient kombiniert werden müssen erhalten Sie ein Produkt von ausgezeichneter Qualität." ',
+            /* FOOTER */
+            philosophy: ' "Wir sind ein Unternehmen, das seine Tätigkeit im Dienstleistungssektor entwickelt, wir widmen uns hauptsächlich der Gestaltung und Produktion von Szenen, audiovisuellen Produkten, Musik und Veranstaltungen, bei denen eine Vielzahl menschlicher und technologischer Faktoren beteiligt sind, die effizient kombiniert werden müssen erhalten Sie ein Produkt von ausgezeichneter Qualität." ',
 
-            /* Under Construction */
+            /* UNDER CONSTRUCTION */
             coomingSoon: "Demnächst",
             working: "Wir arbeiten an unserer Website. Bleiben Sie dran für einige aufregende Updates.",
 
-            /* 404 */
-        },
+            },
         English: {
-            /* Navbar */
+             /* HOME*/
+             fullServiceGrid: "We see ourselves as a full-service provider, i.e. we not only provide the necessary technical equipment, but also ensure that it is perfectly installed for the respective occasion.",
+             verkaufGrid: "Use our free non-binding needs analysis on site. We create your individual solution and give you a good price. Do you prefer an up-to-the-minute new device or would a well-kept used one also do?",
+             vermietungGrid: "Rest assured that the equipment you rent is always in perfect condition. All rental items are serviced regularly. The rental prices do not include statutory VAT, ex our warehouse.",
+             streamingGrid: "We develop and design your individual portal with log-in certification and other technical refinements for a problem-free, attractive stream of your event.",
+             smartHomeGrid: "We automate your private or office building from the central lighting control to a completely smart building. We primarily rely on LOXONE – Home Server and the corresponding peripheral devices.",        
+             prof1: "Professional event...",
+             prof2: "professionally streamed...",
+             prof3: "We not only offer planning and supervision of events, but also rent and/or sell the necessary equipment therefore. Here, too, we are happy to advise you offer high-quality technology at fair prices.",
+            
+             /* BUTTONS & HEADERS*/
             verkauf: "Sale",
             vermietung: "Rental",
             kontakt: "Contact",
             impressum: "Imprint",
             cookieRichtline: "Cookie Policy (EU)",
 
-            /* Footer */
+            /* FOOTER */
             philosophy: ' "We are a company that develops its activity in the service sector, we are mainly dedicated to the design and production of scenes, audiovisual products, music and events that involve a multitude of human and technological factors that must be combined efficiently to obtain a product of excellent quality."',
 
-            /* Under Construction */
+            /* UNDER CONSTRUCTION */
             coomingSoon: "Cooming soon",
             working: "We are working on our website. Stay stuned for some exciting updates.",
 
-            /* 404 */
-        }
+           }
     }
 })
 
