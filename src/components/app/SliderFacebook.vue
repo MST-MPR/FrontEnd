526de005501cb88350662f868d4d392a60d50689
Miguel <migporuiz@gmail.com>
2023-04-24 13:41:27 +0200

    Show images

--- src/components/app/SliderFacebook.vue
+++ src/components/app/SliderFacebook.vue
@@ -29,10 +29,10 @@ export default {
   },
   setup() {
     const onSwiper = (swiper) => {
-      console.log(swiper);
+      //console.log(swiper);
     };
     const onSlideChange = () => {
-      console.log("slide change");
+      //console.log("slide change");
     };
     return {
       onSwiper,
@@ -43,53 +43,66 @@ export default {
   data() {
     return {
       posts: [],
+      images: [],
     };
   },
   mounted() {
-    axios
-      .get("http://127.0.0.1:8000/api/facebookPosts/list")
-      .then((response) => {
-        this.posts = response.data;
-      })
-      .catch((error) => {
+
+    axios.all([
+      axios.get('http://127.0.0.1:8000/api/facebookPosts/list'),
+      axios.get('http://127.0.0.1:8000/api/facebookImages/list', { responseType: 'blob' })
+    ])
+      // Se utiliza la función spread de axios para manejar ambas respuestas de forma separada.
+      .then(axios.spread((facebookPosts, facebookImages) => {
+        console.log(facebookPosts);
+        // Se asigna la respuesta de la petición de los posts a la propiedad "posts" del objeto de datos.
+        this.posts = facebookPosts.data;
+
+        // Se crea una URL temporal para la imagen obtenida en la petición y se asigna a la propiedad "images" del objeto de datos.
+        const imageUrl = URL.createObjectURL(facebookImages.data);
+        this.images = imageUrl;
+        console.log(this.images);
+
+      }))
+      .catch(error => {
         console.log(error);
       });
-  },
+  }
 };
 </script>
 
 <template>
   <Swiper :modules="modules" :slides-per-view="1" @swiper="onSwiper" @slideChange="onSlideChange" :autoplay="{
-    delay: 5000,
-    disableOnInteraction: false,
-  }" effect="fade">
+      delay: 5000,
+      disableOnInteraction: false,
+    }" effect="fade">
     <SwiperSlide v-for="post in posts" :key="post.id">
       <!--<a v-bind:href="post.url" target="_blank">-->
       <div class="text-mst_white h-[600px] md:h-[500px] xl:h-96 flex justify-center items-center">
-        <img src="../../../public/images/General/kino.jpg" class="
-            w-full
-            h-full
-            object-cover
-            absolute
-            mix-blend-overlay
-            object-left-bottom
-            brightness-50
-            blur-sm
-            md:object-center
-          " />
+        <img :src="images" class="
+                    w-full
+                    h-full
+                    object-cover
+                    absolute
+                    mix-blend-overlay
+                    object-left-bottom
+                    brightness-50
+                    blur-sm
+                    md:object-center
+                  " />
         <div class="text-center">
           <h1 class="text-5xl">{{ post.title }}</h1>
           <h2 class="text-xl h-7 md:pr-40 md:text-right md:text-2xl">
             {{ post.date }}
           </h2>
           <p class="
-            text-base
-            p-8
-            tracking-wider
-            leading-relaxed
-            md:px-40 md:text-2xl
-            flex-grow
-          ">
+                    text-base
+                    p-8
+                    tracking-wider
+                    leading-relaxed
+                    md:px-40 md:text-2xl
+                    flex-grow
+                  ">
             {{ post.content }}
           </p>
         </div>
