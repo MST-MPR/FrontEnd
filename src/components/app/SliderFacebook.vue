c862c452684bec7bbb8e94634351fa0ff60e132e
Miguel <20edv14@gmail.com>
2023-05-27 19:16:45 +0200

    Axios SliderFacebook

--- src/components/app/SliderFacebook.vue
+++ src/components/app/SliderFacebook.vue
@@ -43,43 +43,32 @@ export default {
   data() {
     return {
       posts: [],
-      images: [],
     };
   },
   mounted() {
-
-    axios.all([
-      axios.get('http://127.0.0.1:8000/api/facebookPosts/list'),
-      axios.get('http://127.0.0.1:8000/api/facebookImages/list', { responseType: 'blob' })
-    ])
-      // Se utiliza la función spread de axios para manejar ambas respuestas de forma separada.
-      .then(axios.spread((facebookPosts, facebookImages) => {
-        console.log(facebookPosts);
-        // Se asigna la respuesta de la petición de los posts a la propiedad "posts" del objeto de datos.
-        this.posts = facebookPosts.data;
-
-        // Se crea una URL temporal para la imagen obtenida en la petición y se asigna a la propiedad "images" del objeto de datos.
-        const imageUrl = URL.createObjectURL(facebookImages.data);
-        this.images = imageUrl;
-        console.log(this.images);
-
-      }))
+    axios.get('http://127.0.0.1:8000/api/facebookPosts/list')
+      .then(response => {
+        this.posts = response.data;
+        console.log(response.data);
+      })
       .catch(error => {
-        console.log(error);
+        console.error(error);
       });
+
   }
 };
 </script>
 
 <template>
   <Swiper :modules="modules" :slides-per-view="1" @swiper="onSwiper" @slideChange="onSlideChange" :autoplay="{
-      delay: 5000,
-      disableOnInteraction: false,
-    }" effect="fade">
+    delay: 5000,
+    disableOnInteraction: false,
+  }" effect="fade">
     <SwiperSlide v-for="post in posts" :key="post.id">
       <!--<a v-bind:href="post.url" target="_blank">-->
-      <div class="text-mst_white h-[600px] md:h-[500px] xl:h-96 flex justify-center items-center">
-        <img :src="images" class="
+      <div :src="post.urlFacebook" class="text-mst_white h-[600px] md:h-[500px] xl:h-96 flex justify-center items-center">
+        <img class="
+                    
                     w-full
                     h-full
                     object-cover
@@ -91,7 +80,7 @@ export default {
                     md:object-center
                   " />
         <div class="text-center">
-          <h1 class="text-5xl">{{ post.title }}</h1>
+          <h1 class="text-5xl">{{ post.tittle }}</h1>
           <h2 class="text-xl h-7 md:pr-40 md:text-right md:text-2xl">
             {{ post.date }}
           </h2>
